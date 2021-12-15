395e3d5d15ddc65681ac1de2ddd3a503d31e242c
Andrei Gatej <andreigtj01@gmail.com>
2021-12-15 22:50:07 +0200

    feat: add task 4

--- data/setup.sql
+++ data/setup.sql
@@ -1,36 +1,3 @@
-# Online shop database design
-
-* mention how to inspect errors: `show errors`
-
-## Tasks
-
-## 1
-
-> Prezentați pe scurt baza de date (utilitatea ei).
-
-Prezenta baza de date modeleaza un magazin online. In ziua de azi exista numeroase servicii pe Internet ce-si pot dovedi utilitatea doar prin cateva click-uri. Datorita lor, si nu numai, viata oamenilor a devenit mult mai simpla deoarece ei isi pot aloca mai mult timp activitatilor prioritare. Asadar, acest proiect urmareste crearea unei arhitecturi care sta la baza unui astfel de serviciu.
-
-## 2
-
-> Realizați diagrama entitate-relație (ERD).
-
-<div style="text-align: center;">
-  <img src="./images/erd-shop.jpg">
-</div>
-
-## 3
-
-> Pornind de la diagrama entitate-relație realizați diagrama conceptuală a modelului propus, integrând toate atributele necesare.
-
-<div style="text-align: center;">
-  <img src="./images/conceptual-diagram-shop.jpg">
-</div>
-
-## 4
-
-> Implementați în Oracle diagrama conceptuală realizată: definiți toate tabelele, implementând toate constrângerile de integritate necesare (chei primare, cheile externe etc).
-
-```sql
 -- Main entities
 
 create table "user" (
@@ -132,4 +99,3 @@ create table "offer_product" (
   foreign key (product_id)
   references "product"(id)
 );
-```
